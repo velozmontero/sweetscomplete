bf92c0f76c10751bfacd9092ca5cde769f6fb570
velozmontero <velozmontero@gmail.com>
2016-04-20 18:12:45 -0400

    addmember

--- addmember.php
+++ addmember.php
@@ -1,4 +1,177 @@
-<?php // some code ?>
+
+Conversation opened. 2 messages. All messages read.
+
+Skip to content
+Using Gmail with screen readers
+Frank
+Search
+
+
+rferrer@fvi.edu 
+
+
+Gmail
+COMPOSE
+Labels
+Inbox (639)
+Starred
+Important
+Sent Mail
+Drafts
+Circles
+[Imap]/Sent
+[Imap]/Trash (224)
+Notes
+Personal
+Programming
+programming stuff
+Trabajo
+Unwanted
+Viaje
+More labels 
+Hangouts
+
+ 
+ 
+ 
+Move to Inbox More 
+3 of 42  
+ 
+Collapse all Print all In new window
+addmembers.php
+Inbox
+x 
+
+Randy Ferrer
+AttachmentsApr 7 (13 days ago)
+
+to me, julian.albur, kikiqianlin, hgxmiami00, dcardenas09 
+
+Attachments area
+
+Hugo Giraldo
+AttachmentsApr 14 (6 days ago)
+
+to me 
+---------- Forwarded message ----------
+From: Randy Ferrer <rferrer@fvi.edu>
+Date: Thu, Apr 7, 2016 at 7:47 PM
+Subject: addmembers.php
+To: "velozmontero@gmail.com" <velozmontero@gmail.com>, "julian.albur@gmail.com" <julian.albur@gmail.com>, "kikiqianlin@gmail.com" <kikiqianlin@gmail.com>, "hgxmiami00@gmail.com" <hgxmiami00@gmail.com>, "dcardenas09@gmail.com" <dcardenas09@gmail.com>
+
+
+
+Attachments area
+	
+Click here to Reply or Forward
+4.1 GB (27%) of 15 GB used
+Manage
+Terms - Privacy
+Last account activity: 5 minutes ago
+Details
+3 more
+Randy Ferrer
+Add to circles
+
+Show details
+
+
+<?php
+// assign defaults
+$mailStatus = '';
+$data = array('email' 		=> 'email', 
+			  'firstname' 	=> 'firstname', 
+			  'lastname' 	=> 'lastname', 
+			  'address' 	=> 'address', 
+			  'city' 		=> 'city', 
+			  'postcode' 	=> 'postcode', 
+			  'telephone' 	=> 'telephone',
+			  'dobyear'		=> 0,
+			  'dobmonth'	=> 0,
+			  'dobday'		=> 0,
+);
+$error = array('email' 	  => '', 
+			  'firstname' => '', 
+			  'lastname'  => '', 
+			  'address'	  => '', 
+			  'city'	  => '', 
+			  'postcode'  => '', 
+			  'telephone' => '', 
+			  'dob'		  => '',
+);
+if (isset($_POST['data'])) {
+	$data = $_POST['data'];
+	foreach ($data as $key => $value) {
+		$data[$key] = strip_tags($value);
+	}
+	if (isset($data['dobyear']) && isset($data['dobmonth']) && isset($data['dobday'])) {
+		try {
+			$bdateString = sprintf('%4d-%02d-%02d', $data['dobyear'], $data['dobmonth'], $data['dobday']);
+			$bdate = new DateTime($bdateString);
+			$today = new DateTime();
+			$interval21 = new DateInterval('P21Y');
+			$bdate21 = $today->sub($interval21);
+			if ($bdate > $bdate21) $error['dob'] = '<b class="error">Must be at least 21 years old!</b>';
+		} catch (Exception $e) {
+			$error['dob'] = '<b class="error">Invalid date</b>';
+			echo $e->getMessage();
+			exit;
+		}
+		
+	} else {
+		$error['dob'] = '<b class="error">Invalid date</b>';
+	}
+	if (!preg_match('/^[a-z][a-z0-9._-]+@(\w+\.)+[a-z]{2,6}$/i', $data['email'])) {
+		$error['email'] = '<b class="error">Invalid email address</b>';
+	}
+	if (!preg_match('/^[a-z0-9,. ]+$/i', $data['firstname'])) {
+		$error['firstname'] = '<b class="error">Name should only contain letters, numbers, spaces "," or "."</b>';
+	}
+	if (!preg_match('/[a-z0-9,. ]/i', $data['lastname'])) {
+		$error['lastname'] = '<b class="error">Name should only contain letters, numbers, spaces "," or "."</b>';
+	}
+	if (!preg_match('/[a-z0-9,. ]/i', $data['address'])) {
+		$error['address'] = '<b class="error">Address should only contain letters, numbers, spaces "," or "."</b>';
+	}
+	if (!preg_match('/[a-z0-9,. ]/i', $data['city'])) {
+		$error['city'] = '<b class="error">City should only contain letters, numbers, spaces "," or "."</b>';
+	}
+	if (!preg_match('/^[a-z][0-9][a-z] [0-9][a-z][0-9]$|^\d{5}(-\d{4})?$/i', $data['postcode'])) {
+		$error['postcode'] = '<b class="error">Canadian Postcode: A9A 9A9 <br />US Postcodes: 99999 or 99999-9999</b>';
+	}
+	if (!preg_match('/^\+[0-9]{1,3} \d{3}-\d{3}-\d{4}/', $data['telephone'])) {
+		$error['telephone'] = '<b class="error">Telephone numbers should be in form +CC AAA-CCC-DDDD</b>';
+	}
+	// check to see if form valid
+	$isValid = TRUE;
+	foreach ($error as $value) {
+		if ($value) {
+			$isValid = FALSE;
+			break;
+		}
+	}
+	if ($isValid) {
+		require_once('PHPMailer/class.phpmailer.php');
+		$address = "test@unlikelysource.com";
+		$newName = $data['firstname'] . ' ' . $data['lastname'];
+		$mail = new PHPMailer(); // defaults to using php "mail()"
+		$body = 'Welcome to SweetsComplete ' . $newName . '!'
+			  . '<br />To confirm your membership just reply to this email and we\'ll do the rest.'
+			  . '<br />Happy eating!';
+		$mail->AddReplyTo($address,"SweetsComplete");
+		$mail->SetFrom($address,"SweetsComplete");
+		$mail->AddAddress($data['email'], $newName);
+		$mail->Subject = 'SweetsComplete Membership Confirmation';
+		$mail->AltBody = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+		$mail->MsgHTML($body);
+		if(!$mail->Send()) {
+		  $mailStatus = 'Mailer Error: ' . $mail->ErrorInfo;
+		} else {
+		  $mailStatus = 'Confirmation Email Message sent!';
+		}
+	}
+}
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -8,6 +181,10 @@
 <meta name="keywords" content="">
 <link rel="stylesheet" href="css/main.css" type="text/css">
 <link rel="shortcut icon" href="images/favicon.ico?v=2" type="image/x-icon" />
+<style>
+.error   { color: red; 		}
+.confirm { color: green;	}
+</style>
 </head>
 <body>
 <div id="wrapper">
@@ -24,7 +201,7 @@
 			<br />
 			Welcome Guest		</div>
 		<ul class="topmenu">
-		<li><a href="home.php">Home</a></li>
+		<li><a href="index.php">Home</a></li>
 		<li><a href="products.php">Products</a></li>
 		<li><a href="specials.php">Specials</a></li>
 		<li><a href="contact.php">Contact Us</a></li>
@@ -41,35 +218,75 @@
 		<h2>Sign Up</h2>
 		<br/>
 		
-		<b>Please enter your information.</b><br/><br/>
-		<form>
+		<b>Please enter your information.</b>
+		<br/>
+		<br/>
+		<?php if ($mailStatus) echo '<br /><b class="confirm">', $mailStatus, '</b><br />'; ?>
+		<br />
+		<form action="addmember.php" method="POST">
+			<p>
+				<label>Birthdate: </label>
+				<select name="data[dobyear]">
+					<?php if ($data['dobyear']) { echo '<option>', $data['dobyear'], '</option>'; } ?>
+					<?php $year = date('Y'); ?>
+					<?php for($x = $year; $x > ($year - 120); $x--) { ?>
+						<option><?php echo $x; ?></option>
+					<?php }		?>
+				</select>
+				<select name="data[dobmonth]">
+					<?php	
+					$month = array(1 => 'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec');
+					if ($data['dobmonth']) { 
+						printf('<option value="%02d">%s</option>', 
+							   $data['dobmonth'], $month[(int) $data['dobmonth']]);
+					} 
+					for($x = 1; $x <= 12; $x++) {
+						printf('<option value="%02d">%s</option>', $x, $month[$x]);
+						echo PHP_EOL;
+					}		
+					?>
+				</select>
+				<select name="data[dobday]">
+					<?php if ($data['dobday']) { echo '<option>', $data['dobday'], '</option>'; } ?>
+					<?php for($x = 1; $x < 32; $x++) { ?>
+						<option><?php echo $x; ?></option>
+					<?php }		?>
+				</select>
+				<?php if ($error['dob']) echo '<p>', $error['dob']; ?>
 			<p>
 				<label>Email: </label>
-				<input type="text" name="data[email]" />
+				<input type="text" name="data[email]" value="<?php echo htmlspecialchars($data['email']); ?>" />
+				<?php if ($error['email']) echo '<p>', $error['email']; ?>
 			<p>
 			<p>
 				<label>First Name: </label>
-				<input type="text" name="data[firstname]" />
+				<input type="text" name="data[firstname]" value="<?php echo $data['firstname']; ?>" />
+				<?php if ($error['firstname']) echo '<p>', $error['firstname']; ?>
 			<p>
 			<p>
 				<label>Last Name: </label>
-				<input type="text" name="data[lastname]" />
+				<input type="text" name="data[lastname]" value="<?php echo $data['lastname']; ?>" />
+				<?php if ($error['lastname']) echo '<p>', $error['lastname']; ?>
 			<p>
 			<p>
 				<label>Address: </label>
-				<input type="text" name="data[address]" />
+				<input type="text" name="data[address]" value="<?php echo $data['address']; ?>" />
+				<?php if ($error['address']) echo '<p>', $error['address']; ?>
 			<p>
 			<p>
 				<label>City: </label>
-				<input type="text" name="data[city]" />
+				<input type="text" name="data[city]" value="<?php echo $data['city']; ?>" />
+				<?php if ($error['city']) echo '<p>', $error['city']; ?>
 			<p>
 			<p>
 				<label>Postcode: </label>
-				<input type="text" name="data[postcode]" />
+				<input type="text" name="data[postcode]" value="<?php echo $data['postcode']; ?>" />
+				<?php if ($error['postcode']) echo '<p>', $error['postcode']; ?>
 			<p>
 			<p>
 				<label>Telephone: </label>
-				<input type="text" name="data[telephone]" />
+				<input type="text" name="data[telephone]" value="<?php echo $data['telephone']; ?>" />
+				<?php if ($error['telephone']) echo '<p>', $error['telephone']; ?>
 			<p>
 			<p>
 				<input type="reset" name="data[clear]" value="Clear" class="button"/>
@@ -84,7 +301,8 @@
 	<div id="footer">
 		<div class="footer">
 			Copyright &copy; 2012 sweetscomplete.com. All rights reserved. <br/>
-		<a href="home">Home</a> | <a href="products">Products</a> | <a href="specials">Specials</a> | <a href="contact">Contact Us</a> 		<br />
+		<a href="index.php">Home</a> | <a href="products.php">Products</a> | <a href="specials">Specials</a> | <a href="contact">Contact Us</a> 		
+		<br />
 			<span class="contact">Tel: +44-1234567890&nbsp;
 			Fax: +44-1234567891&nbsp;
 			Email:sales@sweetscomplete.com</span>
@@ -95,3 +313,6 @@
 
 </body>
 </html>
+addmember.php
+Open with
+Displaying addmember.php.
